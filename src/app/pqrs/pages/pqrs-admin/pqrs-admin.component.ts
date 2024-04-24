3c8761acb9903a2c59e06d6eb3e93af70e42879d
JavierLo24 <javierandres.123@hotmail.com>
2024-04-24 16:09:17 -0500

    Listar pqrs completa

--- src/app/pqrs/pages/pqrs-admin/pqrs-admin.component.ts
+++ src/app/pqrs/pages/pqrs-admin/pqrs-admin.component.ts
@@ -1,16 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Output, signal } from '@angular/core';
+import { PqrsListComponent } from "../../components/pqrs-list/pqrs-list.component";
+import { PQRS } from '../../api/pqrs';
+import { PqrsService } from '../../services/pqrs.service';
+import { MessageService } from 'primeng/api';
 
 @Component({
-  selector: 'app-pqrs-admin',
-  standalone: true,
-  imports: [],
-  template: `
-    <p>
-      pqrs-admin works!
-    </p>
+    selector: 'app-pqrs-admin',
+    standalone: true,
+    providers: [MessageService],
+    template: `
+        <app-pqrs-list
+            [pqrs]="pqrsLista"
+        ></app-pqrs-list>
   `,
-  styles: ``
+    styles: ``,
+    imports: [PqrsListComponent]
 })
-export class PqrsAdminComponent {
+export class PqrsAdminComponent implements OnInit{
+  pqrsList = signal<PQRS[]>([]);
+
+  constructor(
+    private pqrsService: PqrsService,
+    private message: MessageService
+  ) {
+  }
+
+
+  ngOnInit(): void {
+    this.getPQRSList();
+  }
+
+  get pqrsLista(){
+    return this.pqrsList();
+  }
+
+  getPQRSList(){
+    this.pqrsService.getPqrs().subscribe({
+      next: (res: any) => {
+        this.pqrsList.set(res);
+      },
+      error: (err: any) => {
+        console.log(err)
+      }
+
+    })
+  }
 
 }
